75e522b973706394ad4ac702b13575a5bb0717c5
SamZec <samzec4@gmail.com>
2023-03-10 10:55:10 +0000

    0. Import a simple function from a simple file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from add_0 import add
+
+def _add():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
+
+if __name__ == "__main__":
+    _add()

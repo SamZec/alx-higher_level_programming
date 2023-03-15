e154574f58fe25443bd049f5393347bfda3e66d9
SamZec <samzec4@gmail.com>
2023-03-15 22:27:50 +0000

    9. Multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,7 @@ def multiply_by_2(a_dictionary):
     if not a_dictionary:
         pass
 
+    _dict = dict()
     for i in a_dictionary:
-        a_dictionary[i] *= 2
-    return a_dictionary
+        _dict[i] =  a_dictionary[i] * 2
+    return _dict

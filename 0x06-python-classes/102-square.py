2c8e37daf1a0976fdd64993c0eeab0a06c646ec3
SamZec <samzec4@gmail.com>
2023-03-28 20:34:09 +0000

    9. Compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Defines square attributes"""
+
+    __size = None
+
+    def __init__(self, size=0):
+        "Intantiation of attribute"""
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size**2
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eg__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()

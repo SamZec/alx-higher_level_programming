b41abbb9e747b10952d130b9417624a8f470cfa4
SamZec <samzec4@gmail.com>
2023-03-28 13:28:47 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """Defines square attributes"""
+
+    __size = None
+
+    def __init__(self, size):
+        self.__size = size

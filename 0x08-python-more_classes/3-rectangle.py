211f427b154993ec43bf4f845c5857ca2f0bf883
SamZec <samzec4@gmail.com>
2023-04-03 15:31:25 +0000

    3. String representation

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# 3-rectangle.py
+"""Rectangle Object"""
+
+
+class Rectangle:
+    """Defines a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize Rectangle instance.
+
+            width: The width of the new rectangle.
+            height: The height of the new rectangle.
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """Get/set the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Retunrn the perimeter"""
+        if self.__height != 0 and self.__width != 0:
+            return 2 * (self.__height + self.__width)
+        else:
+            return 0
+
+    def __str__(self):
+        rect = []
+        for i in range(self.__height):
+            rect.append('#' * self.__width)
+        return ('\n'.join(rect))

8ef40fae5aa6dd35d36229e504f812c4153d9957
SamZec <samzec4@gmail.com>
2023-04-04 20:19:53 +0000

    3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 4-print_square.py
+"""Print square"""
+
+
+def print_square(size):
+    """a function that prints a square with the character #.
+        size: the length of square (integer or float) > 0
+    """
+    if ((not isinstance(size, int) and not isinstance(size, float)) or
+            isinstance(size, bool)):
+        raise TypeError('size must be an integer')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    _size = int(size)
+    if _size < 0:
+        raise ValueError('size must be >= 0')
+    if _size == 0:
+        print()
+    else:
+        for i in range(_size):
+            print('#' * _size)

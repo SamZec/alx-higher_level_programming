985310d9992bb5c779359fade350212d79649e06
SamZec <samzec4@gmail.com>
2023-04-04 20:18:32 +0000

    2. Say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# 3-say_my_name.py
+"""
+    A module that prints names
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    a function that prints My name is <first name> <last name>
+
+    first_name: takes first name string
+    last_name: takes last name string or defaut to empty string
+
+    prints: My name is <first name> <last name>
+
+    """
+    if ((not isinstance(first_name, str) and first_name is None)
+            or first_name == ''):
+        raise TypeError('first_name must be a string')
+    if isinstance(first_name, int) or isinstance(first_name, float):
+        raise TypeError('first_name must be a string')
+    if isinstance(last_name, int) or isinstance(last_name, float):
+        raise TypeError('last_name must be a string')
+    if last_name == '':
+        print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))

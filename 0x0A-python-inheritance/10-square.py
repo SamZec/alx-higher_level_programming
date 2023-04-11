a09dad0e70035b778ac1281e719700e04ec0d30b
SamZec <samzec4@gmail.com>
2023-04-11 23:03:53 +0000

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 10-square.py
+"""A module that contains class square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """a class Square that inherits from Rectangle (9-rectangle.py).
+    """
+    def __init__(self, size):
+        """Intantiation od attributes"""
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """Returns the square area"""
+        return self.__size ** 2

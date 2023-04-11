412730daeda2815689c60cc1f192e6da7516afc0
SamZec <samzec4@gmail.com>
2023-04-11 19:19:51 +0000

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""contains lookup funtion for returning list object."""
+
+
+def lookup(obj):
+    """a function that returns the list of available
+        attributes and methods of an objec
+    """
+    return dir(obj)

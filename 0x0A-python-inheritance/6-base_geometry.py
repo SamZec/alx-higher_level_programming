40aa7eec038179867259ef446d803aff6ff99bcb
SamZec <samzec4@gmail.com>
2023-04-11 21:02:14 +0000

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 6-base_geometry.py
+"""A module containing base_geometry class"""
+
+
+class BaseGeometry:
+    """a class that defines a geometry"""
+    def area(self):
+        """not implemented"""
+        raise Exception('area() is not implemented')

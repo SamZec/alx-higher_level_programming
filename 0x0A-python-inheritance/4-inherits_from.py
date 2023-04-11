4f934dec9f11bf66938921d7be126db9f0bdec99
SamZec <samzec4@gmail.com>
2023-04-11 20:32:10 +0000

    4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 4-inherits_from.py
+"""A module containing inherits_from function"""
+
+
+def inherits_from(obj, a_class):
+    """ a function that returns True if the object is an instance
+        of a class that inherited (directly or indirectly) from the
+        specified class ; otherwise False.
+    """
+    if type(obj) is a_class:
+        return False
+    return isinstance(a, a_class)

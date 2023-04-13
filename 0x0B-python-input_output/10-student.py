38f9db585d71f9fa40d431c2a39febf3c9f902b7
SamZec <samzec4@gmail.com>
2023-04-13 01:12:13 +0000

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# 10-student.py
+"""A module for class student that defines a student"""
+
+
+class Student:
+    """a class Student that defines a student by:
+
+        first_name: first name
+        last_name: last name
+        age: age in years
+    """
+    def __init__(self, first_name, last_name, age):
+        """Intantiation of attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) == list and all(isinstance(i, str) for i in attrs):
+            for key in attrs:
+                if hasattr(self, key):
+                    return {key: getattr(self, key)}
+        else:
+            return self.__dict__

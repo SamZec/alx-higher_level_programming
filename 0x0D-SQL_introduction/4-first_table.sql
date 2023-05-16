5eaa2dd12c38b8b2bd9cacca48fa901fd75303d4
SamZec <samzec4@gmail.com>
2023-05-16 13:57:40 +0000

    4. First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- 4-first_table.sql
+-- a script that creates a table called first_table in the current database in your MySQL server.
+
+-- Create table fist_table with field id: INT and name: VARCHAR(250)
+ CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(250));

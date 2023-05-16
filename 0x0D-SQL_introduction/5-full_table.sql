582d013a8f768056aea29260b5cb39f48ff0833c
SamZec <samzec4@gmail.com>
2023-05-16 14:17:11 +0000

    5. Full description

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+-- 5-full_table.sql
+-- a script that prints the full description of the table first_table from the
+-- database hbtn_0c_0 in your MySQL server.
+-- not allowed to use the DESCRIBE or EXPLAIN statements.
+
+-- Full description of first_table
+SHOW CREATE TABLE first_table;

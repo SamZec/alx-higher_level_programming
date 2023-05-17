1642c4bc92868a6ed4e338e5cbf9bd87d21aa1d8
SamZec <samzec4@gmail.com>
2023-05-17 14:41:27 +0000

    5. Unique ID

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- 5-unique_id.sql
+-- a script that creates the table unique_id on your MySQL server.
+CREATE TABLE IF
+	NOT EXISTS
+	unique_id
+	(
+		id INT UNIQUE DEFAULT 1,
+		name VARCHAR(256)
+	);

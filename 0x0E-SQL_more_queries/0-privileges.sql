24816532eb66f5efff5ab9bea14a13d456488cb3
SamZec <samzec4@gmail.com>
2023-05-17 12:34:37 +0000

    0. My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- 0-privileges.sql
+-- a script that lists all privileges of the MySQL users
+-- user_0d_1 and user_0d_2 on your server (in localhost).
+
+-- Show priveleges for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+
+-- Show priveleges for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;

d2047a8db6ea54e5135de7c7b6b935cec6055179
SamZec <samzec4@gmail.com>
2023-05-17 17:51:17 +0000

    12. No genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -3,8 +3,8 @@
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT t.title, g.genre_id
-	FROM tv_shows as t
-		LEFT JOIN tv_show_genres as g
+	FROM tv_shows AS t
+		LEFT JOIN tv_show_genres AS g
 		ON t.id = g.show_id
 		WHERE g.genre_id IS NULL
 	ORDER BY t.title, g.genre_id;

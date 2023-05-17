460f32e754f790a8db0a271ba20f43dfa36cf9a6
SamZec <samzec4@gmail.com>
2023-05-17 14:11:15 +0000

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -9,7 +9,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create user
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY '88@user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant privilege
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;

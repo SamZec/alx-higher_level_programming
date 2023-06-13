f6a1a1c184c39d13a460c3bafd2b706091cf6d0e
SamZec <samzec4@gmail.com>
2023-06-13 00:19:57 +0000

    6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// 6-multi_languages_loop.js - a script that prints 3 lines
+// by using an array of string and a loop
+
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let itr = 0;
+while (itr < 3) {
+  console.log(strArray[itr++]);
+}

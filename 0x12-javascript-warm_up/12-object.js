346592d9a6da088cccd51f08be028fa0842e4588
SamZec <samzec4@gmail.com>
2023-06-13 09:32:44 +0000

    12. Object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// 12-object.js - a script to replace the value 12 with 89
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);

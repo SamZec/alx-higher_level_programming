dfa84eed28b00ba8545aef5a13e43d86367f1a8f
SamZec <samzec4@gmail.com>
2023-06-13 14:50:01 +0000

    5. Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// 5-square.js - script with class Square that defines a square and inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};

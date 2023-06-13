6f1a1e1f205a1a11f28e6a0c77419fc60481f745
SamZec <samzec4@gmail.com>
2023-06-13 16:56:33 +0000

    10. Number conversion

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// 10-converter.js - a script with a function that converts a number from base 10
+// to another base passed as argument
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  };
+};

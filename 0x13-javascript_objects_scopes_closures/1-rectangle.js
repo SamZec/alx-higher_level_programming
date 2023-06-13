1867e852b17cb30ae19c78b22fb6ff3475b72405
SamZec <samzec4@gmail.com>
2023-06-13 13:49:23 +0000

    1. Rectangle #1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// 1-rectangle.js - script with a class Rectangle that defines a rectangle
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};

626c2dac0ebf0dcfcaa40d8dd09f15082da624a0
SamZec <samzec4@gmail.com>
2023-06-13 09:24:51 +0000

    10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,12 +2,13 @@
 // 10-factorial.js - a script that computes and prints a factorial
 
 function factorial (a) {
-  if (isNaN(a))
-    return 1
-  else if (a === 0)
-    return 1
-  else
-    return (a * factorial(a - 1))
+  if (isNaN(a)) {
+    return 1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+    return (a * factorial(a - 1));
+  }
 }
 const arg = process.argv.slice(2);
 const num = parseInt(arg[0]);

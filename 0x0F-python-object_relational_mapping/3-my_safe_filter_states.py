d4bf660e2790b685929b6cd3a6846c0ae99d2ba5
SamZec <samzec4@gmail.com>
2023-06-16 18:28:49 +0000

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,6 +9,7 @@ import sys
 
 
 def List_state():
+    """A function that list states base on arg"""
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     conn = db.cursor()
     conn.execute('SELECT * FROM states ORDER BY id')

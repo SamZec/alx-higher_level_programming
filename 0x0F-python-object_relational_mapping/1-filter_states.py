638e7ad32a4e52f175fc33f4a0d346c3986ad2bf
SamZec <samzec4@gmail.com>
2023-06-17 18:56:55 +0000

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
     1-filter_states.py
-    a script that lists all states from the database hbtn_0e_0_usa
+    a script that lists all states with a name starting
+    with N (upper N) from the database hbtn_0e_0_usa
 """
 
 from sys import argv

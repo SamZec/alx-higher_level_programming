4e224d278506e2e9cf27da5ca4b715e4efd4c3c7
SamZec <samzec4@gmail.com>
2023-06-17 19:25:49 +0000

    2. Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    2-my_filter_states.py
+    a script that takes in an argument and displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def list_states():
+    """A function that list states"""
+    conn = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8"
+            )
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'"
+    cur.execute(query.format(argv[4]))
+    for state in cur.fetchall():
+        print(state)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    list_states()

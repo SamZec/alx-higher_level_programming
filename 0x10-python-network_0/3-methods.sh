e65a89b9bcd1e6d7dd110fd1fc5dd519a144e046
SamZec <samzec4@gmail.com>
2023-06-28 07:12:30 +0000

    3. cURL only methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods allowed by server
-curl -X OPTIONS -sI "$1" | grep 'allow:' | cut -c 8-
+curl -sI "$1" -X OPTIONS | grep "Allow" | cut -c 8-

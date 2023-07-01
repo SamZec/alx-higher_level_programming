de72a8fd8863778d530769660bc395fb47beece7
SamZec <samzec4@gmail.com>
2023-07-01 01:46:05 +0000

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""2-post_email.py - a module for the function postEmail"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+
+def postEmail():
+    """
+    sends a POST request to a URL with an email as a parameter,
+    and displays the body of the response (decoded in utf-8)
+    """
+    data = {'email': '{}'.format(sys.argv[2])}
+    data_parse = urllib.parse.urlencode(data)
+    data_bytes = data_parse.encode('ascii')
+    post = urllib.request.Request(sys.argv[1], data_bytes)
+    with urllib.request.urlopen(post) as fs:
+        print('Your email is: {}'.format(fs.read().decode()))
+
+
+if __name__ == '__main__':
+    postEmail()

e123ccd9084f192925948be28e72a51f9ec1dd4b
SamZec <samzec4@gmail.com>
2023-07-01 00:54:37 +0000

    1. Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-"""1-hbtn_header.py - script that takes in a URL, sends a request to the
-    URL and displays the value of the X-Request-Id variable found in the
-    header of the response.
-"""
+"""1-hbtn_header.py - a module for getId function"""
 
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as fs:
-    f = fs.headers.get('X-Request-Id')
-    print(f)
+def getId():
+    """
+    sends a request to the URL and displays the value of the X-Request-Id
+    variable found in the header of the response.
+    """
+    with urllib.request.urlopen(sys.argv[1]) as fs:
+        f = fs.headers.get('X-Request-Id')
+        print(f)
+
+
+if __name__ == "__main__":
+    getId()

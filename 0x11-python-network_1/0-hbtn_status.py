83a5e55b64bfe2c736fcd0e83ad0cd9342df414a
SamZec <samzec4@gmail.com>
2023-07-01 01:02:10 +0000

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
-"""0-hbtn_status.py - script that fetch https://alx-intranet.hbtn.io/status"""
+"""0-hbtn_status.py - a module for fetchURL function"""
 
 import urllib.request
 
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as fs:
-    f = fs.read()
-    body = "Body response:\n"
-    _type = "    - type: {}\n".format(type(f))
-    content = "    - content: {}\n".format(f)
-    _utf8 = "    - utf8 content: {}".format(f.decode())
-    print(body + _type + content + _utf8)
+def fetchURL():
+    """
+    a function that fetch https://alx-intranet.hbtn.io/status with urllib
+    """
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as fs:
+        f = fs.read()
+        body = "Body response:\n"
+        _type = "    - type: {}\n".format(type(f))
+        content = "    - content: {}\n".format(f)
+        _utf8 = "    - utf8 content: {}".format(f.decode())
+        print(body + _type + content + _utf8)
+
+
+if __name__ == '__main__':
+    fetchURL()

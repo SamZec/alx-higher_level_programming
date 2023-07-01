12d6b8e8c6edfdf8dc70f7f1a8b674cd61784398
SamZec <samzec4@gmail.com>
2023-07-01 00:27:43 +0000

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ import urllib.request
 with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as fs:
     f = fs.read()
     body = "Body response:\n"
-    _type = "    - type {}\n".format(type(f))
-    content = "    - content {}\n".format(f)
+    _type = "    - type: {}\n".format(type(f))
+    content = "    - content: {}\n".format(f)
     _utf8 = "    - utf8 content: {}".format(f.decode())
     print(body + _type + content + _utf8)

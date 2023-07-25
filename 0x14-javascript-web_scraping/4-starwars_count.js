445b2c353b9f73a2dd02815805cc6fbe518cae3e
SamZec <samzec4@gmail.com>
2023-07-25 21:52:49 +0000

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,9 @@ const chrUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 const request = require('request');
 let count = 0;
 request.get(url, function (err, res, body) {
-  if (!err) {
+  if (err) {
+    console.log(err);
+  } else {
     const films = JSON.parse(body);
     for (let i = 0; films.results[i] !== undefined; i++) {
       if (films.results[i].characters.includes(chrUrl)) {
